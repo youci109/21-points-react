4645e2bae20853bfb9be3d2111f99f15a7cfd534
sunxingba <sunxingba@e-u.cn>
2019-07-17 14:15:57 +0800

    实现偏好设置后端接口及测试接口

--- src/main/java/gds/health/repository/PreferencesRepository.java
+++ src/main/java/gds/health/repository/PreferencesRepository.java
@@ -4,6 +4,8 @@ import gds.health.domain.Preferences;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 /**
  * Spring Data  repository for the Preferences entity.
@@ -12,4 +14,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PreferencesRepository extends JpaRepository<Preferences, Long> {
 
+    Optional<Preferences> findOneByUserLogin(String login);
 }
+
